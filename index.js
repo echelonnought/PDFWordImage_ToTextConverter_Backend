8b3d45e45af86c65536e74c5f48b0409abf42cb9
Ngole Lawson <mbwogelawson@gmail.com>
2025-03-12 20:17:20 +0100

    fixing render issues

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ const logger = require('./config/logger');
  // Serve static files from 'public' directory
 
 
- if (config.env !== 'production' || !process.platform === 'linux') {
   const httpServer = http.createServer(app);
-  const server = httpServer.listen(config.port || 3000, () => {
-    logger.info(`Server running at http://localhost:${config.port || 3000}`);
-  })};
+  const server = httpServer.listen(config.port, () => {
+    logger.info(`Server running at http://localhost:${config.port}`);
+  });
 
 
 const exitHandler = () => {
