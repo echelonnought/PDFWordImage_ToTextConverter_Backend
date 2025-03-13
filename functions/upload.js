5219392d0975d1d232233374f7fc90cb99035f22
Ngole Lawson <mbwogelawson@gmail.com>
2025-03-13 11:29:13 +0100

    Remove HTTP server from upload.js for Netlify

--- functions/upload.js
+++ functions/upload.js
@@ -1,40 +1,17 @@
-
 const serverless = require('serverless-http');
 const app = require('../server');
-const http = require('http');
 const config = require('../config/config');
 const logger = require('../config/logger');
 
-// const httpServer = http.createServer(app);
-// const server = httpServer.listen(config.port || 5000, () => {
-// logger.info(`Server running at http://localhost:${config.port}`);
-// });
+const unExpectedErrorHandler = (error) => {
+  logger.error('Unhandled error:', error);
+  throw error;
+};
 
+process.on('uncaughtException', unExpectedErrorHandler);
+process.on('unhandledRejection', unExpectedErrorHandler);
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM received');
+});
 
-const exitHandler = () => {
-    if (server) {
-      server.close(() => {
-        console.log('Server closed');
-        process.exit(1);
-      });
-    } else {
-      process.exit(1);
-    }
-  };
-   
-  const unExpectedErrorHandler = (error) => {
-    logger.error(error);
-    exitHandler();
-  };
-   
-  process.on('uncaughtException', unExpectedErrorHandler);
-  process.on('unhandledRejection', unExpectedErrorHandler);
-  process.on('SIGTERM', () => {
-    logger.info('SIGTERM received');
-    if (server) {
-      server.close();
-    }
-  });
-
-// Export as serverless function
 module.exports.handler = serverless(app);
