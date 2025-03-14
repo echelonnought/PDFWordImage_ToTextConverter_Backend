d2af36bd45027ac2db6a284cc1441e399a8efdd7
Ngole Lawson <mbwogelawson@gmail.com>
2025-03-14 17:32:40 +0100

    fixed paths

--- functions/upload.js
+++ functions/upload.js
@@ -1,17 +1,7 @@
 const serverless = require('serverless-http');
 const app = require('../server');
-const config = require('../config/config');
-const logger = require('../config/logger');
 
-const unExpectedErrorHandler = (error) => {
-  logger.error('Unhandled error:', error);
-  throw error;
-};
 
-process.on('uncaughtException', unExpectedErrorHandler);
-process.on('unhandledRejection', unExpectedErrorHandler);
-process.on('SIGTERM', () => {
-  logger.info('SIGTERM received');
-});
+
 
 module.exports.handler = serverless(app);
